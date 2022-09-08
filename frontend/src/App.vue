512087a5739eb06e095d9a0dd378a1e14f44b0c5
ggpp200 <ggpp20000@gmail.com>
2022-09-08 09:49:36 +0900

    style: front merge
    기본틀 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
-    <kakaoMap></kakaoMap>
+    <router-view></router-view>
+    <router-link to="/cctv">sadfsdfdasf</router-link>
     <div>{{store.count}}</div>
   </div>
 </template>
 
 <script>
 import { useIndexStore } from "@/stores/index"
-import kakaoMap from '@/components/kakaoMap'
 export default {
   name: 'App',
   components: {
-    kakaoMap,
-  },
+},
   setup () {
     const store = useIndexStore()
-    store.count++ 
+    store.count ++
     return {
       store
     }
@@ -24,12 +23,17 @@ export default {
 </script>
 
 <style>
+:root {
+  --main-color1: #000000;
+  --main-color2: #eee;
+  --main-color3: #FFFFFF;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/system/control/ObjectItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ObjectItemBox">123</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.ObjectItemBox {
+    width: 320px;
+    height: 60px;
+}
+</style>
