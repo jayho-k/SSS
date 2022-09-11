afedf36feeeb63e834de4e8e7a00ea39b70e6283
장종훈 <jangjr98@naver.com>
2022-09-11 15:24:35 +0900

    [BE] feat : user_activate checkuser함수(access토큰을 통해 user_id반환) 및 user_activate 개발

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    Name = models.CharField(max_length=10)
-    phonenumber = models.CharField(max_length=13)
+    name = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=13)
     department = models.CharField(max_length=20)
     is_admin = models.BooleanField(default=False)
-    activation = models.BooleanField(default=False)
+    activation = models.IntegerField(default=0)
