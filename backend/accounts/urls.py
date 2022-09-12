98847eabc3432b7c0f67c9719b25117311a41cf3
장종훈 <jangjr98@naver.com>
2022-09-12 19:17:26 +0900

    [BE] feat : login_custom 로그인을 했을경우 username과 password가 일치하는지 확인하고 activation = 1이며 로그인중인지 확인하도록 개발

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("user/", views.user_detail_or_update_or_delete),
     # JWT 토큰 발행
-    path('user/login/', TokenObtainPairView.as_view()),
+    path('user/login/', views.login),
     # refresh토큰으로 유저 확인후 access토큰 재발행
     path('user/token/reissuance/', TokenRefreshView.as_view()),
     # 유효성 검사 기능(왜 쓰는지 모르겠음)
