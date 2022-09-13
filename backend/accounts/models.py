1504812abcf79c2efb4616d9c80bbf08bd29c3a0
장종훈 <jangjr98@naver.com>
2022-09-13 14:41:17 +0900

    [BE] feat : DB cctv,공공안전,미아찾기 DB생성

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
@@ -7,4 +8,4 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=13)
     department = models.CharField(max_length=20)
     is_admin = models.BooleanField(default=False)
-    activation = models.IntegerField(default=0)
+    activation = models.IntegerField(default=0,validators=[MinValueValidator(-1), MaxValueValidator(1)])
