e41de7f9638a12c75f5a40a81f442a0ea8c98f76
Seung Jun <jsznawa@gmail.com>
2022-09-14 15:06:02 +0900

    backend delete

--- backend/cctvs/urls.py
+++
@@ -1,7 +0,0 @@
-from accounts.views import password_change
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("/cctvs", views.cctv_detail_or_create_or_update_or_delete),
-]
--- backend/uploads/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.upload_save_or_delete),
-    path("result/", views.upload_result_save)
-]
