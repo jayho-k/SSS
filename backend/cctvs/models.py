cd2078ecb351d466658af44efea005ca6252875d
Seung Jun <jsznawa@gmail.com>
2022-09-14 14:56:24 +0900

    [BE] feat : CCTV

--- backend/cctvs/models.py
+++ backend/cctvs/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-class Cctv(models.Model):
+class CCTV(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     video = models.CharField(max_length=20)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    activation = models.BooleanField()
+    activation = models.BooleanField(default=True)
     
     
