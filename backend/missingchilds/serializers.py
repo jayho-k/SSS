ac0273d523ff8f294cf7b7efe68786d6ef29767c
장종훈 <jangjr98@naver.com>
2022-09-15 17:48:51 +0900

    [BE] fix : image 이미지 폴더 저장 함수 변경

--- backend/missingchilds/serializers.py
+++ backend/missingchilds/serializers.py
@@ -4,6 +4,7 @@ from .models import Child
 User = get_user_model()
 
 class ChildSerializer(serializers.ModelSerializer):
+    child_img = serializers.ImageField(required=False)
     class Meta:
         model = Child
         fields = (
