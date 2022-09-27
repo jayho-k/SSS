003e78f221b39ce0fb48b0895ad26b4579492418
장종훈 <jangjr98@naver.com>
2022-09-27 14:58:44 +0900

    [BE] feat : upload 동영상 저장후 반환

--- backend/cctvs/models.py
+++ backend/cctvs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from backend.common import file_upload_path
 # Create your models here.
 
 class CCTV(models.Model):
@@ -8,3 +9,8 @@ class CCTV(models.Model):
     video = models.CharField(max_length=20)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    
+    
+class Upload(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    video_file = models.FileField(upload_to=file_upload_path)
