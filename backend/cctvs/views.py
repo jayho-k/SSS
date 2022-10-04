6b4f8da60ff89280569e16948b253ec72b2f96c9
김찬일 <kci8311@gmail.com>
2022-10-04 16:09:40 +0900

    Update backend/cctvs/views.py, backend/.gitignore

--- backend/cctvs/views.py
+++ backend/cctvs/views.py
@@ -220,7 +220,7 @@ def upload(request):
                 yolo_weights= WEIGHTS / 'yolov7.pt',  # model.pt path(s),
                 strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
                 config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-                device=0,  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+                device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
                 deepsort=True, ########### MOT or not custumized variable ########################
                 project=TRACK,  # save results to project/name
                 name=name_exp,  # save results to project/name
