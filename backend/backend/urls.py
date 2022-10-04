f5fd922f4c216b53e7a039424f268e62d2ea656f
장종훈 <jangjr98@naver.com>
2022-10-04 16:52:50 +0900

    [BE] fix : backend 에러 수정

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("cctvs/", include("cctvs.urls")),
     path("missingchilds/", include("missingchilds.urls")),
-    path("uploads/", include("uploads.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
