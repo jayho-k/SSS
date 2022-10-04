bb315151736844eeab7dd141e0b3de6b9f22ada6
chanil <kci8311@gmail.com>
2022-10-04 16:39:13 +0900

    [AI] fix: code editting

--- backend/yolo7deep/yolo_api.py
+++ backend/yolo7deep/yolo_api.py
@@ -45,6 +45,99 @@ from strong_sort.strong_sort import StrongSORT
 
 VID_FORMATS = 'asf', 'avi', 'gif', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'ts', 'wmv'  # include video suffixes
 
+def yolo_detect_api(**kwargs):
+    api = parse_api(**kwargs)
+    run(**api)
+
+def parse_api(
+        source=ROOT / 'test_video.mp4',
+        yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
+        strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+        config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        show_vid=False,  # show results
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        save_vid=False,  # save confidences in --save-txt labels
+        nosave=False,  # do not save images/videos
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs/track',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        hide_class=False,  # hide IDs
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+        deepsort=False #MOT or not custumized thing
+):
+    api = {}
+    api['yolo_weights'] = yolo_weights
+    api['strong_sort_weights'] = strong_sort_weights
+    api['config_strongsort'] = config_strongsort
+    api['source'] = source
+    api['device'] = device
+    api['deepsort'] = deepsort
+    api['classes'] = classes
+    api['project'] = project
+    api['name'] = name
+    api['save_vid'] = save_vid
+    ########################################################
+    api['imgsz'] = imgsz
+    api['conf_thres'] = conf_thres
+    api['iou_thres'] = iou_thres
+    api['max_det'] = max_det
+    api['show_vid'] = show_vid
+    api['save_txt'] = save_txt
+    api['save_conf'] = save_conf
+    api['save_crop'] = save_crop
+    api['nosave'] = nosave
+    api['line_thickness'] = line_thickness
+    api['hide_labels'] = hide_labels
+    api['hide_conf'] = hide_conf
+    # source='0',
+    # yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
+    # strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+    # config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
+    # imgsz=(640, 640),  # inference size (height, width)
+    # conf_thres=0.25,  # confidence threshold
+    # iou_thres=0.45,  # NMS IOU threshold
+    # max_det=1000,  # maximum detections per image
+    # device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    # show_vid=False,  # show results
+    # save_txt=False,  # save results to *.txt
+    # save_conf=False,  # save confidences in --save-txt labels
+    # save_crop=False,  # save cropped prediction boxes
+    # save_vid=False,  # save confidences in --save-txt labels
+    # nosave=False,  # do not save images/videos
+    # classes=None,  # filter by class: --class 0, or --class 0 2 3
+    # agnostic_nms=False,  # class-agnostic NMS
+    # augment=False,  # augmented inference
+    # visualize=False,  # visualize features
+    # update=False,  # update all models
+    # project=ROOT / 'runs/track',  # save results to project/name
+    # name='exp',  # save results to project/name
+    # exist_ok=False,  # existing project/name ok, do not increment
+    # line_thickness=3,  # bounding box thickness (pixels)
+    # hide_labels=False,  # hide labels
+    # hide_conf=False,  # hide confidences
+    # hide_class=False,  # hide IDs
+    # half=False,  # use FP16 half-precision inference
+    # dnn=False,  # use OpenCV DNN for ONNX inference
+    api['imgsz'] *= 2 if len(api['imgsz']) == 1 else 1  # expand
+
+    return api
+
 
 @torch.no_grad()
 def run(
@@ -308,97 +401,3 @@ def run(
     if update:
         strip_optimizer(yolo_weights)  # update model (to fix SourceChangeWarning)
     return save_dir
-
-def parse_api(
-        source=ROOT / 'test_video.mp4',
-        yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
-        strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
-        config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        show_vid=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        save_vid=False,  # save confidences in --save-txt labels
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'runs/track',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        hide_class=False,  # hide IDs
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-        deepsort=False #MOT or not custumized thing
-):
-    api = {}
-    api['yolo_weights'] = yolo_weights
-    api['strong_sort_weights'] = strong_sort_weights
-    api['config_strongsort'] = config_strongsort
-    api['source'] = source
-    api['device'] = device
-    api['deepsort'] = deepsort
-    api['classes'] = classes
-    api['project'] = project
-    api['name'] = name
-    api['save_vid'] = save_vid
-    ########################################################
-    api['imgsz'] = imgsz
-    api['conf_thres'] = conf_thres
-    api['iou_thres'] = iou_thres
-    api['max_det'] = max_det
-    api['show_vid'] = show_vid
-    api['save_txt'] = save_txt
-    api['save_conf'] = save_conf
-    api['save_crop'] = save_crop
-    api['nosave'] = nosave
-    api['line_thickness'] = line_thickness
-    api['hide_labels'] = hide_labels
-    api['hide_conf'] = hide_conf
-    # source='0',
-    # yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
-    # strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
-    # config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-    # imgsz=(640, 640),  # inference size (height, width)
-    # conf_thres=0.25,  # confidence threshold
-    # iou_thres=0.45,  # NMS IOU threshold
-    # max_det=1000,  # maximum detections per image
-    # device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-    # show_vid=False,  # show results
-    # save_txt=False,  # save results to *.txt
-    # save_conf=False,  # save confidences in --save-txt labels
-    # save_crop=False,  # save cropped prediction boxes
-    # save_vid=False,  # save confidences in --save-txt labels
-    # nosave=False,  # do not save images/videos
-    # classes=None,  # filter by class: --class 0, or --class 0 2 3
-    # agnostic_nms=False,  # class-agnostic NMS
-    # augment=False,  # augmented inference
-    # visualize=False,  # visualize features
-    # update=False,  # update all models
-    # project=ROOT / 'runs/track',  # save results to project/name
-    # name='exp',  # save results to project/name
-    # exist_ok=False,  # existing project/name ok, do not increment
-    # line_thickness=3,  # bounding box thickness (pixels)
-    # hide_labels=False,  # hide labels
-    # hide_conf=False,  # hide confidences
-    # hide_class=False,  # hide IDs
-    # half=False,  # use FP16 half-precision inference
-    # dnn=False,  # use OpenCV DNN for ONNX inference
-    api['imgsz'] *= 2 if len(api['imgsz']) == 1 else 1  # expand
-
-    return api
-
-
-def yolo_detect_api(**kwargs):
-    api = parse_api(**kwargs)
-    run(**api)
