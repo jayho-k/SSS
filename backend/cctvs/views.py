892e928154dd1dec6aa3fa98116ca69ea1929ef1
jayho-k <sox3579@gmail.com>
2022-10-05 21:39:28 +0900

    [AI] feat : slowfast video upload
    Former-commit-id: 92c7646ed35bca7c0f5c7516386c76c4b239fdda

--- backend/cctvs/views.py
+++ backend/cctvs/views.py
@@ -111,10 +111,9 @@ def cctv_detail_or_update_or_delete(request):
 @gzip.gzip_page
 @renderer_classes((StreamingHttpResponse))
 def streaming(request,user_id,cctv_id,type):
-    # user = get_object_or_404(get_user_model(), id=user_id)
-    # addr = get_object_or_404(CCTV, id=cctv_id)
-    addr = 0
-
+    user = get_object_or_404(get_user_model(), id=user_id)
+    addr = get_object_or_404(CCTV, id=cctv_id)
+    
     if type == "fire" or type == "mia":
         cam = video_camera(addr, type)
         FILE = Path(__file__).resolve()
@@ -243,79 +242,86 @@ def upload(request):
         print(type(request.FILES["video"]))
         print(upload.video_file.path.split("\\")[-1])
         upload.save()
-        FILE = Path(__file__).resolve() 
+        FILE = Path(__file__).resolve()
+        
         ROOT = FILE.parents[0].parents[0] / 'yolo7deep'  # yolov5 strongsort root directory
         WEIGHTS = ROOT / 'weights'
         TRACK = ROOT.parents[0] /'media/track'
         name_exp = 'exp'
-        if request.data.get("class") == "mia":
-            print(WEIGHTS)
-            yolo_api.yolo_detect_api(
-                source=upload.video_file.path,
-                yolo_weights= WEIGHTS / 'yolov7.pt',  # model.pt path(s),
-                strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
-                config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-                device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-                deepsort=True, ########### MOT or not custumized variable ########################
-                project=TRACK,  # save results to project/name
-                name=name_exp,  # save results to project/name
-                save_vid=True,  # save confidences in --save-txt labels
-                classes=[0,1],  # filter by class: --class 0, or --class 0 2 3
-                # line_thickness=3,  # bounding box thickness (pixels)
-                # conf_thres=0.25,  # confidence threshold
-                # iou_thres=0.45,  # NMS IOU threshold
-                # save_crop=False,  # save cropped prediction boxes
-                ###############################################################
-                # show_vid=False,  # show results
-                # imgsz=(640, 640),  # inference size (height, width)
-                # max_det=1000,  # maximum detections per image
-                # save_txt=False,  # save results to *.txt
-                # save_conf=False,  # save confidences in --save-txt labels
-                # nosave=False,  # do not save images/videos
-                # exist_ok=False,  # existing project/name ok, do not increment
-            )
-        elif request.data.get("class") == "fire":
-            # 화재 모델
-            yolo_api.yolo_detect_api(
-                source=upload.video_file.path,
-                # yolo_weights= WEIGHTS / 'yolov7.pt',  # model.pt path(s),
-                yolo_weights= WEIGHTS / 'fire.pt',  # model.pt path(s),
-                strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
-                config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-                device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-                deepsort=False, ########### MOT or not custumized variable ########################
-                project=TRACK,  # save results to project/name
-                name=name_exp,  # save results to project/name
-                save_vid=True,  # save confidences in --save-txt labels
-                conf_thres=0.5,  # confidence threshold
-                # classes=[0,1]  # filter by class: --class 0, or --class 0 2 3
-                # line_thickness=3,  # bounding box thickness (pixels)
-                # iou_thres=0.45,  # NMS IOU threshold
-                # save_crop=False,  # save cropped prediction boxes
-                ###############################################################
-                # show_vid=False,  # show results
-                # imgsz=(640, 640),  # inference size (height, width)
-                # max_det=1000,  # maximum detections per image
-                # save_txt=False,  # save results to *.txt
-                # save_conf=False,  # save confidences in --save-txt labels
-                # nosave=False,  # do not save images/videos
-                # agnostic_nms=False,  # class-agnostic NMS
-                # augment=False,  # augmented inference
-                # visualize=False,  # visualize features
-                # update=False,  # update all models
-                # exist_ok=False,  # existing project/name ok, do not increment
-                # hide_labels=False,  # hide labels
-                # hide_conf=False,  # hide confidences
-                # hide_class=False,  # hide IDs
-                # half=False,  # use FP16 half-precision inference
-                # dnn=False,  # use OpenCV DNN for ONNX inference
-            )
-        elif request.data.get("class") == "safety":
-            # 공공안전
-            test_run.run(upload.video_file.path)
+        if request.data.get("class") == "mia" or request.data.get("class") == "fire":
 
-
+            if request.data.get("class") == "mia":
+                print(WEIGHTS)
+                yolo_api.yolo_detect_api(
+                    source=upload.video_file.path,
+                    yolo_weights= WEIGHTS / 'yolov7.pt',  # model.pt path(s),
+                    strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+                    config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
+                    device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+                    deepsort=True, ########### MOT or not custumized variable ########################
+                    project=TRACK,  # save results to project/name
+                    name=name_exp,  # save results to project/name
+                    save_vid=True,  # save confidences in --save-txt labels
+                    classes=[0,1],  # filter by class: --class 0, or --class 0 2 3
+                    # line_thickness=3,  # bounding box thickness (pixels)
+                    # conf_thres=0.25,  # confidence threshold
+                    # iou_thres=0.45,  # NMS IOU threshold
+                    # save_crop=False,  # save cropped prediction boxes
+                    ###############################################################
+                    # show_vid=False,  # show results
+                    # imgsz=(640, 640),  # inference size (height, width)
+                    # max_det=1000,  # maximum detections per image
+                    # save_txt=False,  # save results to *.txt
+                    # save_conf=False,  # save confidences in --save-txt labels
+                    # nosave=False,  # do not save images/videos
+                    # exist_ok=False,  # existing project/name ok, do not increment
+                )
+            elif request.data.get("class") == "fire":
+                # 화재 모델
+                yolo_api.yolo_detect_api(
+                    source=upload.video_file.path,
+                    # yolo_weights= WEIGHTS / 'yolov7.pt',  # model.pt path(s),
+                    yolo_weights= WEIGHTS / 'fire.pt',  # model.pt path(s),
+                    strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
+                    config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
+                    device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+                    deepsort=False, ########### MOT or not custumized variable ########################
+                    project=TRACK,  # save results to project/name
+                    name=name_exp,  # save results to project/name
+                    save_vid=True,  # save confidences in --save-txt labels
+                    conf_thres=0.5,  # confidence threshold
+                    # classes=[0,1]  # filter by class: --class 0, or --class 0 2 3
+                    # line_thickness=3,  # bounding box thickness (pixels)
+                    # iou_thres=0.45,  # NMS IOU threshold
+                    # save_crop=False,  # save cropped prediction boxes
+                    ###############################################################
+                    # show_vid=False,  # show results
+                    # imgsz=(640, 640),  # inference size (height, width)
+                    # max_det=1000,  # maximum detections per image
+                    # save_txt=False,  # save results to *.txt
+                    # save_conf=False,  # save confidences in --save-txt labels
+                    # nosave=False,  # do not save images/videos
+                    # agnostic_nms=False,  # class-agnostic NMS
+                    # augment=False,  # augmented inference
+                    # visualize=False,  # visualize features
+                    # update=False,  # update all models
+                    # exist_ok=False,  # existing project/name ok, do not increment
+                    # hide_labels=False,  # hide labels
+                    # hide_conf=False,  # hide confidences
+                    # hide_class=False,  # hide IDs
+                    # half=False,  # use FP16 half-precision inference
+                    # dnn=False,  # use OpenCV DNN for ONNX inference
+                )
             
+
+        elif request.data.get("class") == "safety":
+            # FILE_ROOT = FILE.parents[0].parents[0]
+            # 공공안전
+            res = upload.video_file.path.split("\\")[-1]
+            input_path = upload.video_file.path
+            output_path = str(TRACK / f'exp/{res}')
+            test_run.run(input_path=input_path,output_path=output_path)
+
         videos = Upload.objects.filter(user=user_id)
         idx = videos.count()
         video = videos[idx - 1]
@@ -325,6 +331,7 @@ def upload(request):
         file = FileWrapper(open(f'media/track/exp/{res}', mode='rb'))
         print('ddddddddddddddddddddd', file)
         # file = FileWrapper(open(f'media/video/20220930/{res}', mode='rb'))
+
         response = HttpResponse(file, content_type='video/mp4')
         # response['Content-Disposition'] = 'attachment; filename=result_video.mp4'
 
